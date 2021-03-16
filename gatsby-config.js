0c62f7b38f810cebbdfe4e9de745b08fb06e4af4
data-88 <andrew@thecyberium.com>
2021-03-16 12:48:48 +0200

    services

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 module.exports = {
   siteMetadata: {
     siteTitle: `Make up by Marcelle`,
-    siteTitleAlt: `Make up by Marcelle`,
+    siteTitleAlt: `Marcelle`,
   },
   plugins: [
     {
