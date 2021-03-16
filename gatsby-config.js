7ca60d977e1ff046af17019cdca2bedabc9d1551
data-88 <andrew@thecyberium.com>
2021-03-16 12:21:34 +0200

    posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `Make up by Marcelle`,
   },
   plugins: [
     {
@@ -41,9 +41,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Make up by Marcelle`,
+        short_name: `Marcelle`,
+        description: `Make up services in Johannesburg`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
